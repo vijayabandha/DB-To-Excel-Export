6fdebd5e2a82f3db442dcc34fa2cbc9f510bac61
vijaya <vijayabandha22@gmail.com>
2023-04-13 12:42:18 +0530

    initial commit

---
+++ SpringbootExcelAppApplication.java
@@ -0,0 +1,13 @@
+package in.ashokit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootExcelAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootExcelAppApplication.class, args);
+	}
+
+}
